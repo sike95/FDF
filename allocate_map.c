27724e42141fc6ccb8572dd0e638ba73be1aa1ba
Michael MPOFU <mmpofu@e4r18p3.wethinkcode.co.za>
2017-10-10 09:57:49 +0200

    delete

--- allocate_map.c
+++ allocate_map.c
@@ -6,7 +6,7 @@
 /*   By: mmpofu <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/06 09:30:02 by mmpofu            #+#    #+#             */
-/*   Updated: 2017/10/07 11:38:46 by mmpofu           ###   ########.fr       */
+/*   Updated: 2017/10/08 18:03:08 by mmpofu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,32 @@
 
 t_row_col		get_size(int fd)
 {
-	t_row_col  size;
-	int		flag;
-	int		i;
-	int		cnt;
-	char	**split;
-	char	*line;
+	t_get_size	var;
 
-	i = 0;
-	cnt = 0;
-	flag = 0;
-	while ((get_next_line(fd, &line)) > 0)
+	var.i = 0;
+	var.cnt = 0;
+	var.flag = 0;
+	while ((get_next_line(fd, &var.line)) > 0)
 	{
-		if (flag == 0)
+		if (var.flag == 0)
 		{
-			split = ft_strsplit(line, ' ');
-			while(split[cnt])
-				cnt++;
-			flag = 1;
+			var.split = ft_strsplit(var.line, ' ');
+			while (var.split[var.cnt])
+				var.cnt++;
+			var.flag = 1;
 		}
-		i++;
-		free(line);
+		var.i++;
 	}
-	free(line);
-	size.row = i;
-	size.col = cnt;
-	return (size);
+	free(var.line);
+	var.size.row = var.i;
+	var.size.col = var.cnt;
+	return (var.size);
 }
 
-int     allocate_map(t_row_col   size, int ***temp_map)
+int				allocate_map(t_row_col size, int ***temp_map)
 {
-	int     y;
-	
-//	printf("row %d col %d", size.row, size.col);
+	int			y;
+
 	y = 0;
 	(*temp_map) = (int**)malloc(sizeof(int*) * (size.row + 1));
 	while (y < size.row)
@@ -57,7 +50,7 @@ int     allocate_map(t_row_col   size, int ***temp_map)
 	return (1);
 }
 
-int		populate_map(int fd, int ***map, t_row_col   size)
+int				populate_map(int fd, int ***map, t_row_col size)
 {
 	int			i;
 	int			j;
@@ -65,8 +58,7 @@ int		populate_map(int fd, int ***map, t_row_col   size)
 	char		*line;
 
 	i = 0;
-	//printf("%d, %d \n", size.row, size.col);
-	while ((get_next_line(fd, &line)) > 0 && i < size.row)
+	while ((get_next_line(fd, &line)) > 0)
 	{
 		split = ft_strsplit(line, ' ');
 		j = 0;
@@ -75,10 +67,34 @@ int		populate_map(int fd, int ***map, t_row_col   size)
 			(*map)[i][j] = ft_atoi(split[j]);
 			j++;
 		}
-	//		printf("\n");
 		i++;
 		free(line);
 	}
-//	free(line);
 	return (1);
+}
+
+void			connecter(void *mlx, void *win, t_map p0, t_map p1)
+{
+	t_vector	vec;
+	t_move		step;
+	double		crawl;
+	int			i;
+
+	if ((vec.d.x = (p1.x - p0.x))
+			> (vec.d.y = (p1.y - p0.y)))
+		crawl = fabs(vec.d.x);
+	else
+		crawl = fabs(vec.d.y);
+	vec.mv.x = vec.d.x / crawl;
+	vec.mv.y = vec.d.y / crawl;
+	step.x = p0.x;
+	step.y = p0.y;
+	i = 0;
+	while (i < crawl)
+	{
+		step.x = step.x + vec.mv.x;
+		step.y = step.y + vec.mv.y;
+		mlx_pixel_put(mlx, win, (int)step.x, (int)step.y, 0x00FFFF00);
+		i++;
+	}
 }
